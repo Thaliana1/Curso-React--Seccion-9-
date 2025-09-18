cb2407171db325e2b26f98b10043844ae5333877
Thaliana <thalianavanessa2@gmail.com>
2025-09-18 17:01:23 -0500

    Primer commit

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+
+import './index.css'
+
+
+//import { HooksApp } from './04-hooks-app/HooksApp';
+//import { TrafficLight } from './02-useEffect/TrafficLight';
+//import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
+//import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
+import { PokemonPage } from './03-examples/PokemonPages';
+//import {FocusScreen} from './04-useRef/FocusScreen';
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+
+   {/*<HooksApp/>*/}
+   {/*<TrafficLight/>*/}
+   {/*<TrafficLightWithEffect/>*/}
+   {/*<TrafficLightWithHook/>/*/}
+    <PokemonPage/>
+   {/*<FocusScreen/>*/}
+
+  </StrictMode>
+)
