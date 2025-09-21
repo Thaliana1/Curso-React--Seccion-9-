7c2500631c354fefd82ee4cd62af354a0535e1dd
Thaliana <thalianavanessa2@gmail.com>
2025-09-21 16:25:30 -0500

    Actualización: cambios de la seccion 10

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css'
 
 
+
 //import { HooksApp } from './04-hooks-app/HooksApp';
 //import { TrafficLight } from './02-useEffect/TrafficLight';
 //import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
 //import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
 //import { PokemonPage } from './03-examples/PokemonPages';
-import {FocusScreen} from './04-useRef/FocusScreen';
+//import {FocusScreen} from './04-useRef/FocusScreen';
+//import { TasksApp } from './05-useReducer/TaskApp';
+import { ScrambleWords } from './05-useReducer/ScrambleWords';
 
 
 createRoot(document.getElementById('root')!).render(
@@ -21,8 +24,11 @@ createRoot(document.getElementById('root')!).render(
    {/*<TrafficLightWithEffect/>*/}
    {/*<TrafficLightWithHook/>/*/}
    {/*<PokemonPage/>*/}
-   
-   <FocusScreen/>
+   {/*<FocusScreen/>*/}
+   {/*< TasksApp />*/}
+   <ScrambleWords/>
+
+
 
   </StrictMode>
 )
